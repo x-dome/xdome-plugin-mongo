d507bcf62241fd734ad631b537a212bde916ff3b
Joel De La Torriente <jjdltc@gmail.com>
2020-08-16 21:07:41 +0100

    chore: Initial commit

---
+++ resources/scripts/devAliase.sh
@@ -0,0 +1,7 @@
+echo "Remember to 'source' this files"
+echo "-----------------------------------------"
+echo "Setting Up mocha and eslint alias"
+alias mocha=`pwd`/node_modules/mocha/bin/mocha
+alias eslint=`pwd`/node_modules/eslint/bin/eslint.js
+echo "mocha and eslint alias are active on this session"
+echo "-----------------------------------------"
